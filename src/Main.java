2e90302411ca9c7c3d9d10d0518776d41da1e362
David <dsxvqq@gmail.com>
2023-10-02 21:25:08 +0600

    fixed packages

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+package src;
+
 import java.util.Scanner;
 
 /*
@@ -5,6 +7,7 @@ import java.util.Scanner;
  * @author shinemettd (David O.)
  *
  */
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
